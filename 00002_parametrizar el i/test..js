5cb59b64836640bf74af5ace29293a6afd29f081
andres@digitalhouse.com <andres@digitalhouse.com>
2018-04-19 16:42:59 -0400

    Mumuki Export on 2018-04-19 16:42:59 -0400

--- 00002_parametrizar el i/test..js
+++ 00002_parametrizar el i/test..js
@@ -1,6 +1,6 @@
 describe("imprimirAzul", () => {
   it("imprimirAzul(3) es \"AzulAzulAzul\"", () => {
     imprimirAzul(1);
-    assert.equal(console.toString(), console.toString())
+    assert.equal(console.toString(), "Azul")
   })
 })
