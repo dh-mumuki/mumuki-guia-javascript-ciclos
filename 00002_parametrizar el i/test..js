c64b8834d7b263c7e65c56342314a4a64da7f65e
andres@digitalhouse.com <andres@digitalhouse.com>
2018-04-19 16:38:47 -0400

    Mumuki Export on 2018-04-19 16:38:47 -0400

--- 00002_parametrizar el i/test..js
+++ 00002_parametrizar el i/test..js
@@ -1,14 +1,6 @@
-describe("decisionConMoneda", () => {
-  it("decisionConMoneda(\"cara\", \"pizzas\", \"empanadas\") es \"pizzas\"", () => {
-    assert.equal(decisionConMoneda("cara", "pizzas", "empanadas"), "pizzas")
-  })
-  it("decisionConMoneda(\"cara\", \"asado\", \"empanadas\") es \"asado\"", () => {
-    assert.equal(decisionConMoneda("cara", "asado", "empanadas"), "asado")
-  })
-  it("decisionConMoneda(\"ceca\", \"pizzas\", \"empanadas\") es \"empanadas\"", () => {
-    assert.equal(decisionConMoneda("ceca", "pizzas", "empanadas"), "empanadas")
-  })
-  it("decisionConMoneda(\"ceca\", \"pizzas\", \"pastas\") es \"pastas\"", () => {
-    assert.equal(decisionConMoneda("ceca", "pizzas", "pastas"), "pastas")
+describe("imprimirAzul", () => {
+  it("imprimirAzul(3) es \"AzulAzulAzul\"", () => {
+    imprimirAzul(3)
+    assert.equal(console.toString(), "AzulAzulAzul")
   })
 })
