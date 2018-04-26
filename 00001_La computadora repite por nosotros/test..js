c564ae4d2975401a1f79f70a621b6e95048779f2
andres@digitalhouse.com <andres@digitalhouse.com>
2018-04-26 14:55:46 -0400

    Mumuki Export on 2018-04-26 14:55:46 -0400

--- 00001_La computadora repite por nosotros/test..js
+++ 00001_La computadora repite por nosotros/test..js
@@ -1,5 +1,5 @@
 describe("", function() {
   it("deberia imprimir cuatro veces la palabra Azul", function() {
-    assert.ok(fakeconsole.toString()=== "AzulAzulAzulAzul")
+    assert.ok(fakeConsole.toString()=== "AzulAzulAzulAzul")
   });
 })
