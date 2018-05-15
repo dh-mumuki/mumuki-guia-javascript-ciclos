ca40a0557237697113df5437112f92a72cd93bf3
andres@digitalhouse.com <andres@digitalhouse.com>
2018-05-15 19:19:31 -0400

    Mumuki Export on 2018-05-15 19:19:31 -0400

--- 00001_La computadora repite por nosotros/test..js
+++ 00001_La computadora repite por nosotros/test..js
@@ -1,5 +1,5 @@
-describe("Imprimir Azul", function() {
-  it("deberia imprimir Azul 4 veces", function() {
+describe("versos", function() {
+  it("deberia imprimir los versos", function() {
     imprimirAzul4();
     assert.equal(console.toString(), "Azul\nAzul\nAzul\nAzul\n")
   });
