8a6971fcc14200505f6c777d17fda89642e9a8fa
andres@digitalhouse.com <andres@digitalhouse.com>
2018-05-15 19:51:05 -0400

    Mumuki Export on 2018-05-15 19:51:05 -0400

--- 00004_Muchas Empanadas/test..js
+++ 00004_Muchas Empanadas/test..js
@@ -4,8 +4,8 @@ describe("Imprimir sumarMonedasDe25(5)", function() {
     assert.equal(console.toString(), "1.25\n")
   });
     it("deberia imprimir por pantalla 1.50", function() {
-    sumarMonedasDe25(6);
-    assert.equal(console.toString(), "1.5\n")
+    sumarMonedasDe25(5);
+    assert.equal(console.toString(), "1.25\n")
   });
 })
 
