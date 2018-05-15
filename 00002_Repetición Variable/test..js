a62f22abe9a057975e769416e9c53a109d368a5d
andres@digitalhouse.com <andres@digitalhouse.com>
2018-05-15 19:34:19 -0400

    Mumuki Export on 2018-05-15 19:34:19 -0400

--- 00002_Repetición Variable/test..js
+++ 00002_Repetición Variable/test..js
@@ -1,7 +1,7 @@
 describe("Imprimir imprimirAzul(3)", function() {
   it("deberia imprimir por pantalla Azul Azul Azul", function() {
     imprimirAzul(3);
-    assert.equal(console.toString(), "Azul\n\Azul\n3\Azul\n")
+    assert.equal(console.toString(), "Azul\n\Azul\n\Azul\n")
   });
 })
 
