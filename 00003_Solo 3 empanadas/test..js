bc581be1aaccbaee4bc6a6c9c458115f4dac0486
andres@digitalhouse.com <andres@digitalhouse.com>
2018-05-15 19:36:55 -0400

    Mumuki Export on 2018-05-15 19:36:55 -0400

--- 00003_Solo 3 empanadas/test..js
+++ 00003_Solo 3 empanadas/test..js
@@ -1,7 +1,7 @@
 describe("Imprimir imprimirAzul(3)", function() {
   it("deberia imprimir por pantalla Azul Azul Azul", function() {
     sumatoriaHasta4();
-    assert.equal(console.toString(), "7\n")
+    assert.equal(console.toString(), "5\n")
   });
 })
 
