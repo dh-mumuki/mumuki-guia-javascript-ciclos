9d9351961dbf22b85a2aa1098fab9e6406e823fa
andres@digitalhouse.com <andres@digitalhouse.com>
2018-05-15 19:21:20 -0400

    Mumuki Export on 2018-05-15 19:21:20 -0400

--- 00005_Pasito a pasito.../test..js
+++ 00005_Pasito a pasito.../test..js
@@ -1,6 +1,6 @@
 describe("Imprimir PasitoAPasito()", function() {
   it("deberia imprimir la secuencia 01234", function() {
-    imprimirAzul4();
+    pasitoApasito();
     assert.equal(console.toString(), "0\n1\n2\n3\n4\n")
   });
 })
