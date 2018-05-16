be15c61b5c1ffe48844340876ca6e23e34192aaa
andres@digitalhouse.com <andres@digitalhouse.com>
2018-05-15 20:08:28 -0400

    Mumuki Export on 2018-05-15 20:08:28 -0400

--- 00006_Sumatoria/test..js
+++ 00006_Sumatoria/test..js
@@ -1,5 +1,7 @@
-describe("Group description", function() {
-  it("Sample description", function() {
-    assert(true)
-  })
+describe("Imprimir sumatoriaHasta(4)", function() {
+  it("deberia imprimir por pantalla 6", function() {
+    sumatoriaHasta(4);
+    assert.equal(console.toString(), "6\n")
+  });
 })
+
