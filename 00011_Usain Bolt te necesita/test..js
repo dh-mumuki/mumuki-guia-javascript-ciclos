dff0599d5675eb0ce99ef9ad46b07a765d4416e0
andres@digitalhouse.com <andres@digitalhouse.com>
2018-05-23 12:45:17 -0400

    Mumuki Export on 2018-05-23 12:45:17 -0400

--- 00011_Usain Bolt te necesita/test..js
+++ 00011_Usain Bolt te necesita/test..js
@@ -1,6 +1,6 @@
-describe("Imprimir sumatoriaHasta(4)", function() {
-  it("deberia imprimir por pantalla 6", function() {
-    sumatoriaHasta(4);
+describe("Imprimir caloriasDeTrote(30)", function() {
+  it("deberia imprimir por pantalla 35", function() {
+    caloriasDeTrote(35);
     assert.equal(console.toString(), "6\n")
   });
 })
