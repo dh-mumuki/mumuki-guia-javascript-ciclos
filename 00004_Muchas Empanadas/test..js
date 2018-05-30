c802385caa7f1868a2816c6a470f4d78971eb135
andres@digitalhouse.com <andres@digitalhouse.com>
2018-05-30 17:13:25 -0400

    Mumuki Export on 2018-05-30 17:13:25 -0400

--- 00004_Muchas Empanadas/test..js
+++ 00004_Muchas Empanadas/test..js
@@ -1,11 +1,11 @@
 describe("Calcular sumarMonedasDe25(6)", function() {
   it("deberia devolver 1.50", function() {
-    assert.equal(sumarMonedasDe25(6), 1.5")
+    assert.equal(sumarMonedasDe25(6), 1.5)
   });
 })
 describe("Calcular sumarMonedasDe25(4)", function() {
   it("deberia devolver 1", function() {
-    assert.equal(sumarMonedasDe25(4), 1")
+    assert.equal(sumarMonedasDe25(4), 1)
   });
 })
 
